638e4742f8d196c9942eaa7d59b6fb33cc0a4aee
M Afzaal Afzal <afzaal1721@gmail.com>
2021-10-17 00:18:59 -0700

    basic site completed

---
+++ components/home/FeatureSection.js
@@ -0,0 +1,210 @@
+import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, MailIcon, ScaleIcon } from '@heroicons/react/outline'
+
+const transferFeatures = [
+    {
+        id: 1,
+        name: 'Turn-key Device Onboarding',
+        description:`Wether it’s a Z-wave Hub, WiFi device, or Matter connected device we make it easy to onboard your devices into our platform.`,
+        icon: GlobeAltIcon,
+    },
+    {
+        id: 2,
+        name: 'Seamless Developer Experience',
+        description:`Build scalable apps with the top developer experience in the world. Top notch documentation, 3 minute tutorials, open source examples, and pre-made reciepies for 1 click deploys.`,
+           icon: ScaleIcon,
+    },
+    {
+        id: 3,
+        name: 'Unified API, SDK’s. and Developer Tools',
+        description: `A common interface to integrate with thousands of connected smart-home devices. Easy to use SDK’s for the top programming languages. Developer tools to track events, debug, and see logs.`,
+             icon: LightningBoltIcon,
+    },
+]
+const communicationFeatures = [
+    {
+        id: 1,
+        name: 'AI Enabled Insights',
+        description:  `Using our advanced AI we are constantly looking for errors and issues. Pre-emptive scans can help detect problems before they become real issues. Performance problems.. or connection issues... or cell network issues...`,
+              icon: AnnotationIcon,
+    },
+    {
+        id: 2,
+        name: 'Self-healing',
+        description: `Using our event intelligence we use ability of devZix to self-correct problems and potential problems before they become noticeable to end users`,
+        icon: MailIcon,
+    },
+]
+
+export default function FeatureSection() {
+    return (
+        <div className="py-16 bg-gray-50 overflow-hidden lg:py-24">
+            <div className="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+                <svg
+                    className="hidden lg:block absolute left-full transform -translate-x-1/2 -translate-y-1/4"
+                    width={404}
+                    height={784}
+                    fill="none"
+                    viewBox="0 0 404 784"
+                    aria-hidden="true"
+                >
+                    <defs>
+                        <pattern
+                            id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7"
+                            x={0}
+                            y={0}
+                            width={20}
+                            height={20}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                        </pattern>
+                    </defs>
+                    <rect width={404} height={784} fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
+                </svg>
+
+                <div className="relative">
+                    <h2 className="text-center text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                        A better way to integrate smart-home devices
+                    </h2>
+                    <p className="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">
+                        Developer-first API’s create a seamless integration experience
+                    </p>
+                </div>
+
+                <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:gap-8 lg:items-center">
+                    <div className="relative">
+                        <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+                            Build best of breed apps not integrations
+                        </h3>
+                        <p className="mt-3 text-lg text-gray-500">
+                            xxxxxxxx
+                        </p>
+
+                        <dl className="mt-10 space-y-10">
+                            {transferFeatures.map((item) => (
+                                <div key={item.id} className="relative">
+                                    <dt>
+                                        <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                            <item.icon className="h-6 w-6" aria-hidden="true" />
+                                        </div>
+                                        <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{item.name}</p>
+                                    </dt>
+                                    <dd className="mt-2 ml-16 text-base text-gray-500">{item.description}</dd>
+                                </div>
+                            ))}
+                        </dl>
+                    </div>
+
+                    <div className="mt-10 -mx-4 relative lg:mt-0" aria-hidden="true">
+                        <svg
+                            className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                            width={784}
+                            height={404}
+                            fill="none"
+                            viewBox="0 0 784 404"
+                        >
+                            <defs>
+                                <pattern
+                                    id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                                    x={0}
+                                    y={0}
+                                    width={20}
+                                    height={20}
+                                    patternUnits="userSpaceOnUse"
+                                >
+                                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                                </pattern>
+                            </defs>
+                            <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
+                        </svg>
+                        <img
+                            className="relative mx-auto"
+                            width={490}
+                            src="https://tailwindui.com/img/features/feature-example-1.png"
+                            alt=""
+                        />
+                    </div>
+                </div>
+
+                <svg
+                    className="hidden lg:block absolute right-full transform translate-x-1/2 translate-y-12"
+                    width={404}
+                    height={784}
+                    fill="none"
+                    viewBox="0 0 404 784"
+                    aria-hidden="true"
+                >
+                    <defs>
+                        <pattern
+                            id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
+                            x={0}
+                            y={0}
+                            width={20}
+                            height={20}
+                            patternUnits="userSpaceOnUse"
+                        >
+                            <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                        </pattern>
+                    </defs>
+                    <rect width={404} height={784} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+                </svg>
+
+                <div className="relative mt-12 sm:mt-16 lg:mt-24">
+                    <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:gap-8 lg:items-center">
+                        <div className="lg:col-start-2">
+                            <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">Observability done right</h3>
+                            <p className="mt-3 text-lg text-gray-500">
+                                Our platform provides immersive event logging, error reporting, and advanced debugging into your connect mesh networks, z-wave hubs, etc..
+                            </p>
+
+                            <dl className="mt-10 space-y-10">
+                                {communicationFeatures.map((item) => (
+                                    <div key={item.id} className="relative">
+                                        <dt>
+                                            <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                                                <item.icon className="h-6 w-6" aria-hidden="true" />
+                                            </div>
+                                            <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{item.name}</p>
+                                        </dt>
+                                        <dd className="mt-2 ml-16 text-base text-gray-500">{item.description}</dd>
+                                    </div>
+                                ))}
+                            </dl>
+                        </div>
+
+                        <div className="mt-10 -mx-4 relative lg:mt-0 lg:col-start-1">
+                            <svg
+                                className="absolute left-1/2 transform -translate-x-1/2 translate-y-16 lg:hidden"
+                                width={784}
+                                height={404}
+                                fill="none"
+                                viewBox="0 0 784 404"
+                                aria-hidden="true"
+                            >
+                                <defs>
+                                    <pattern
+                                        id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
+                                        x={0}
+                                        y={0}
+                                        width={20}
+                                        height={20}
+                                        patternUnits="userSpaceOnUse"
+                                    >
+                                        <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                                    </pattern>
+                                </defs>
+                                <rect width={784} height={404} fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
+                            </svg>
+                            <img
+                                className="relative mx-auto"
+                                width={490}
+                                src="https://tailwindui.com/img/features/feature-example-2.png"
+                                alt=""
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,149 @@
-import Head from 'next/head'
+/*
+  This example requires Tailwind CSS v2.0+
 
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  This example requires some changes to your config:
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+  ```
+  // tailwind.config.js
+  const colors = require('tailwindcss/colors')
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+  module.exports = {
+    // ...
+    theme: {
+      extend: {
+        colors: {
+          teal: colors.teal,
+          cyan: colors.cyan,
+        },
+      },
+    },
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+import {Fragment} from 'react'
+import {Popover, Transition} from '@headlessui/react'
+import {
+    CloudUploadIcon,
+    CogIcon,
+    LockClosedIcon,
+    MenuIcon,
+    RefreshIcon,
+    ServerIcon,
+    ShieldCheckIcon,
+    XIcon,
+} from '@heroicons/react/outline'
+import {ChevronRightIcon, ExternalLinkIcon} from '@heroicons/react/solid'
+import HeroSection from "../components/home/HeroSection";
+import FeatureSection from "../components/home/FeatureSection";
+import OpenEndedPossibilitiesSection from "../components/home/OpenEndedPossibilitiesSection";
+import SignUpSection from "../components/home/SignUpSection";
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+const features = [
+    {
+        name: 'Push to Deploy',
+        description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi vitae lobortis.',
+        icon: CloudUploadIcon,
+    },
+    {
+        name: 'SSL Certificates',
+        description: 'Qui aut temporibus nesciunt vitae dicta repellat sit dolores pariatur. Temporibus qui illum aut.',
+        icon: LockClosedIcon,
+    },
+    {
+        name: 'Simple Queues',
+        description: 'Rerum quas incidunt deleniti quaerat suscipit mollitia. Amet repellendus ut odit dolores qui.',
+        icon: RefreshIcon,
+    },
+    {
+        name: 'Advanced Security',
+        description: 'Ullam laboriosam est voluptatem maxime ut mollitia commodi. Et dignissimos suscipit perspiciatis.',
+        icon: ShieldCheckIcon,
+    },
+    {
+        name: 'Powerful API',
+        description:
+            'Ab a facere voluptatem in quia corrupti veritatis aliquam. Veritatis labore quaerat ipsum quaerat id.',
+        icon: CogIcon,
+    },
+    {
+        name: 'Database Backups',
+        description: 'Quia qui et est officia cupiditate qui consectetur. Ratione similique et impedit ea ipsum et.',
+        icon: ServerIcon,
+    },
+]
+const blogPosts = [
+    {
+        id: 1,
+        title: 'Boost your conversion rate',
+        href: '#',
+        date: 'Mar 16, 2020',
+        datetime: '2020-03-16',
+        category: {name: 'Article', href: '#'},
+        imageUrl:
+            'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+        preview:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+        author: {
+            name: 'Roel Aufderehar',
+            imageUrl:
+                'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            href: '#',
+        },
+        readingLength: '6 min',
+    },
+    {
+        id: 2,
+        title: 'How to use search engine optimization to drive sales',
+        href: '#',
+        date: 'Mar 10, 2020',
+        datetime: '2020-03-10',
+        category: {name: 'Video', href: '#'},
+        imageUrl:
+            'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+        preview:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
+        author: {
+            name: 'Brenna Goyette',
+            imageUrl:
+                'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            href: '#',
+        },
+        readingLength: '4 min',
+    },
+    {
+        id: 3,
+        title: 'Improve your customer experience',
+        href: '#',
+        date: 'Feb 12, 2020',
+        datetime: '2020-02-12',
+        category: {name: 'Case Study', href: '#'},
+        imageUrl:
+            'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+        preview:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
+        author: {
+            name: 'Daniela Metz',
+            imageUrl:
+                'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+            href: '#',
+        },
+        readingLength: '11 min',
+    },
+]
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+export default function HomePage() {
+    return (
+       <div>
+           <HeroSection/>
+           <FeatureSection/>
+           <OpenEndedPossibilitiesSection/>
+           <SignUpSection/>
+       </div>
+    )
 }
