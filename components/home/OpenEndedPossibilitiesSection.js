73df3896bb0c25947801b5248c5b340189a79df1
M Afzaal Afzal <afzaal1721@gmail.com>
2021-10-18 10:07:33 +0500

    images are added

--- components/home/OpenEndedPossibilitiesSection.js
+++ components/home/OpenEndedPossibilitiesSection.js
@@ -9,14 +9,16 @@ const OpenEndedPossibilitiesSection = () => {
         <>
             {/* Alternating Feature Sections */}
             <div className="relative pt-16 pb-32 overflow-hidden">
-                <div aria-hidden="true" className="absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-gray-100" />
+                <div aria-hidden="true" className="absolute inset-x-0 top-0 h-48 bg-gradient-to-b from-gray-100"/>
                 <div className="relative">
-                    <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+                    <div
+                        className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
                         <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
                             <div>
                                 <div>
-                    <span className="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600">
-                      <InboxIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    <span
+                        className="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600">
+                      <InboxIcon className="h-6 w-6 text-white" aria-hidden="true"/>
                     </span>
                                 </div>
                                 <div className="mt-6">
@@ -24,7 +26,8 @@ const OpenEndedPossibilitiesSection = () => {
                                         Open-ended possibilities
                                     </h2>
                                     <p className="mt-4 text-lg text-gray-500">
-                                        Our open API provides the best in class developer friendly way to connect smart home devices to get your applications out to market faster and with less errors
+                                        Our open API provides the best in class developer friendly way to connect smart
+                                        home devices to get your applications out to market faster and with less errors
                                     </p>
                                     <div className="mt-6">
                                         <a
@@ -40,7 +43,8 @@ const OpenEndedPossibilitiesSection = () => {
                                 <blockquote>
                                     <div>
                                         <p className="text-base text-gray-500">
-                                            &ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci risus aenean
+                                            &ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci
+                                            risus aenean
                                             curabitur donec aliquet. Mi venenatis in euismod ut.&rdquo;
                                         </p>
                                     </div>
@@ -64,8 +68,8 @@ const OpenEndedPossibilitiesSection = () => {
                         <div className="mt-12 sm:mt-16 lg:mt-0">
                             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
                                 <img
-                                    className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
-                                    src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
+                                    className="w-full rounded-xl  lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                                    src="/home/api.png"
                                     alt="Inbox user interface"
                                 />
                             </div>
@@ -73,12 +77,15 @@ const OpenEndedPossibilitiesSection = () => {
                     </div>
                 </div>
                 <div className="mt-24">
-                    <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-                        <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+                    <div
+                        className="lg:mx-auto lg:max-w-7xl place-items-center lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+                        <div
+                            className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
                             <div>
                                 <div>
-                    <span className="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600">
-                      <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    <span
+                        className="h-12 w-12 rounded-md flex items-center justify-center bg-gradient-to-r from-purple-600 to-indigo-600">
+                      <SparklesIcon className="h-6 w-6 text-white" aria-hidden="true"/>
                     </span>
                                 </div>
                                 <div className="mt-6">
@@ -86,8 +93,10 @@ const OpenEndedPossibilitiesSection = () => {
                                         Better understand your customers
                                     </h2>
                                     <p className="mt-4 text-lg text-gray-500">
-                                        Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia
-                                        porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at.
+                                        Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada
+                                        faucibus lacinia
+                                        porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium
+                                        pharetra at.
                                         Lectus viverra dui tellus ornare pharetra.
                                     </p>
                                     <div className="mt-6">
@@ -102,10 +111,10 @@ const OpenEndedPossibilitiesSection = () => {
                             </div>
                         </div>
                         <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
-                            <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                            <div className="pr-4 lg:px-0 lg:m-0 lg:relative lg:h-full">
                                 <img
-                                    className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
-                                    src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
+                                    className="w-full sm:max-w-[460px] mx-auto h-auto lg:w-auto lg:max-w-none"
+                                    src="/home/router.png"
                                     alt="Customer profile user interface"
                                 />
                             </div>
